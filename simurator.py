97a8c449907e0089bdff86b767c7c27b0055bc60
tazawaakira <a.tazawa.sys24@morijyobi.ac.jp>
2025-02-14 16:23:40 +0900

    管理メニューの再修正 #24

--- simurator.py
+++ simurator.py
@@ -41,6 +41,7 @@ def Management_screen():
     print(f'***********************\n管理画面\n***********************\n\n======= 商品一覧 =======\n\n商品      単価  販売数  売上金額\n\n=======================\n1.特製ラーメン 1000円  {temp_dep[0]}   {(menu[0][1])*(temp_dep[0])}円\n2.醤油ラーメン 780円   {temp_dep[1]}     {(menu[1][1])*(temp_dep[1])}\n3.しおラーメン 880円     {temp_dep[2]}     {(menu[2][1])*(temp_dep[2])}円\n4.ごはん 150円      {temp_dep[3]}        {(menu[3][1])*(temp_dep[3])}円\n\n\n———\n総売上金額 {(menu[0][1])*(temp_dep[0])+(menu[1][1])*(temp_dep[1])+(menu[2][1])*(temp_dep[2])+(menu[3][1])*(temp_dep[3])}円')
     print('=== 管理メニュー===\n1.売上をリセットする\n2.商品の価格を変更する\n※売上がリセットされていないと利用できません。\n3.管理画面を終了する')
     mana_code = int(input("管理コード入力:"))
+    print(mana_code)
     while True:
         if mana_code == 1:
             temp_dep = [0,0,0,0]
@@ -49,13 +50,15 @@ def Management_screen():
             break
         elif mana_code == 2:
             print(f"======= 商品一覧 =======\n\n商品      単価  販売数  売上金額\n\n=======================\n1.特製ラーメン 1000円  {temp_dep[0]}   {(menu[0][1])*(temp_dep[0])}円\n2.醤油ラーメン 780円   {temp_dep[1]}     {(menu[1][1])*(temp_dep[1])}\n3.しおラーメン 880円     {temp_dep[2]}     {(menu[2][1])*(temp_dep[2])}円\n4.ごはん 150円      {temp_dep[3]}        {(menu[3][1])*(temp_dep[3])}円\n\n\n———\n総売上金額 {(menu[0][1])*(temp_dep[0])+(menu[1][1])*(temp_dep[1])+(menu[2][1])*(temp_dep[2])+(menu[3][1])*(temp_dep[3])}円")
+            print('aaaaa')
             num = int(input("価格を変更する商品の番号を入力して下さい。>"))
             price  = int(input("変更金額を入力してください。"))
             y_or_n = input(f'【{num}.{menu[num - 1][0]} {price}円】に変更します。\nよろしいですか(Y/N) >')
             if y_or_n == 'y':
-                menu[num - 1][1]=price
+                menu[num - 1][1] = price
+                print(f'{num}.{menu[num - 1][1]}')
                 print("変更しました。")
-                print(f"======= 商品一覧 =======\n\n商品      単価  販売数  売上金額\n\n=======================\n1.特製ラーメン 1000円  {temp_dep[0]}   {(menu[0][1])*(temp_dep[0])}円\n2.醤油ラーメン 780円   {temp_dep[1]}     {(menu[1][1])*(temp_dep[1])}\n3.しおラーメン 880円     {temp_dep[2]}     {(menu[2][1])*(temp_dep[2])}円\n4.ごはん 150円      {temp_dep[3]}        {(menu[3][1])*(temp_dep[3])}円\n\n\n———\n総売上金額 {(menu[0][1])*(temp_dep[0])+(menu[1][1])*(temp_dep[1])+(menu[2][1])*(temp_dep[2])+(menu[3][1])*(temp_dep[3])}円")
+                print(f"======= 商品一覧 =======\n\n商品      単価  販売数  売上金額\n\n=======================\n1.特製ラーメン {menu[0][1]}円  {temp_dep[0]}   {(menu[0][1])*(temp_dep[0])}円\n2.醤油ラーメン {menu[1][1]}円   {temp_dep[1]}     {(menu[1][1])*(temp_dep[1])}\n3.しおラーメン {menu[2][1]}円     {temp_dep[2]}     {(menu[2][1])*(temp_dep[2])}円\n4.ごはん {menu[3][1]}円      {temp_dep[3]}        {(menu[3][1])*(temp_dep[3])}円\n\n\n———\n総売上金額 {(menu[0][1])*(temp_dep[0])+(menu[1][1])*(temp_dep[1])+(menu[2][1])*(temp_dep[2])+(menu[3][1])*(temp_dep[3])}円")
                 break
         elif mana_code == 3:
             print('**********************\n券売機シミュレーター\n**********************')
